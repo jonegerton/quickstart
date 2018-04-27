ea66f919dff027841edf23754fe9ee0f78e75055
Jon <jonegerton@ja2.co.uk>
2018-04-27 17:10:07 +0100

    routing and dash pt 1

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,16 +1,40 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+
 import 'src/heroes_component.dart';
+import 'src/dashboard_component.dart';
 import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
-  directives: const [HeroesComponent],
+  directives: const [ROUTER_DIRECTIVES],
   providers: const [HeroService],
   template: '''
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   ''',
 )
+
+@RouteConfig(const [
+  const Route(
+      path: '/heroes',
+      name: 'Heroes',
+      component: HeroesComponent,
+  ),
+  const Route(
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+  ),
+  const Redirect(
+      path: '/',
+      redirectTo: const ['Dashboard']
+  ),
+])
 
 class AppComponent {
   final title = 'Tour of heroes';
