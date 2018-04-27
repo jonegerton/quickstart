07735916356c15a70486b4209c0207d8e403295f
Jon <jonegerton@ja2.co.uk>
2018-04-27 15:29:25 +0100

    multiple components

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,11 +1,11 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
 import 'src/hero.dart';
 import 'src/mock_heroes.dart';
+import 'src/hero_detail_component.dart';
 
 @Component(
   selector: 'my-app',
-  directives: const [CORE_DIRECTIVES, formDirectives],
+  directives: const [CORE_DIRECTIVES, HeroDetailComponent],
   templateUrl: 'app_component.html',
   styleUrls: const ['app_component.css'],
 )
