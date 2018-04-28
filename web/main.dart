93c0eee635512d721f51a3169c87efb14864e94f
Jon <jonegerton@ja2.co.uk>
2018-04-28 13:25:20 +0100

    http pt1

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,16 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_app/app_component.dart';
+//import 'package:http/browser_client.dart';
+import 'package:angular_app/in_memory_data_service.dart';
+import 'package:http/http.dart';
 
 void main() {
   bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     // Remove next line in production
     provide(LocationStrategy, useClass: HashLocationStrategy),
+    //provide(Client, useFactory: () => new BrowserClient(), deps: [])
+    provide(Client, useClass: InMemoryDataService)
   ]);
 }
